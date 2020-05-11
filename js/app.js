53dd8887e4ad4e63d893132531f5ba01e5b1dc10
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-05-11 17:55:22 -0400

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var cells = document.getElementsByTagName(`td`).item(0);
+
+    for (var cell of cells) {
+    cell.addEventListener(`click`, rotate)
+}
+     function rotate() {
+        document.getElementsByTagName(`A`).item(0);
+        document.getElementsByTagName(`B`).item(0);
+        document.getElementsByTagName(`C`).item(0);
+        document.getElementsByTagName(`D`).item(0);
+    }
+
