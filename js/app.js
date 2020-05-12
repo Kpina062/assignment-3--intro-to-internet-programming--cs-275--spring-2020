dc633bbfe8ad072f4051befdf95d0a14e57d366b
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-05-12 11:49:40 -0400

    Update attribute to className

--- js/app.js
+++ js/app.js
@@ -10,15 +10,15 @@ cells.forEach(function(t) {
         cells.forEach(function(c) {
             if (c !== t){
                 if (rotStatus[i] === 1) {
-                    c.setAttribute(`style`,`transform:rotate(0deg)`);
+                    c.className=`rotation`;
                     rotStatus[i] = 0;
                 }
             } else {
                 if (rotStatus[i] === 0) {
-                    t.setAttribute(`style`,`transform:rotate(180deg)`);
+                    t.className=`rotation`;
                     rotStatus[i] = 1;
                 } else {
-                    t.setAttribute(`style`,`transform:rotate(0deg)`);
+                    t.className=`rotation`;
                     rotStatus[i] = 0;
                 }
             }
