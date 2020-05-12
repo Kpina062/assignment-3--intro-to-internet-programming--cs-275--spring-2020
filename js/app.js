6cefaa8b002d0eed533298dfe340ddf38ed77dc4
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-05-12 14:13:29 -0400

    Update className

--- js/app.js
+++ js/app.js
@@ -1,28 +1,30 @@
-const rotStatus = [];
-const cells = document.querySelectorAll("td");
-for (let i = 0; i < cells.length; i++) {
-    rotStatus.push(0);
-}
+window.onload=()=> {
+    let rotStatus = [];
+    let cells = document.querySelectorAll(`td`);
+    for (let i = 0; i < cells.length; i++) {
+        rotStatus.push(0);
+    }
 
-cells.forEach(function(t) {
-    t.onclick = function () {
-        let i = 0;
-        cells.forEach(function(c) {
-            if (c !== t){
-                if (rotStatus[i] === 1) {
-                    c.className=`rotation`;
-                    rotStatus[i] = 0;
-                }
-            } else {
-                if (rotStatus[i] === 0) {
-                    t.className=`rotation`;
-                    rotStatus[i] = 1;
+    cells.forEach(function (t) {
+        t.onclick = function () {
+            let i = 0;
+            cells.forEach(function (c) {
+                if (c !== t) {
+                    if (rotStatus[i] === 1) {
+                        c.className=`rotation2`;
+                        rotStatus[i] = 0;
+                    }
                 } else {
-                    t.className=`rotation`;
-                    rotStatus[i] = 0;
+                    if (rotStatus[i] === 0) {
+                       t.className=`rotation1`;
+                        rotStatus[i] = 1;
+                    } else {
+                        t.className=`rotation2`;
+                        rotStatus[i] = 0;
+                    }
                 }
-            }
-            i++;
-        });
-    };
-});
+                i++;
+            });
+        };
+    });
+};
