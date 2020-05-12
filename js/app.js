d8a76e081554b091825987b61fba0bff927ec392
Kpina062 <60191521+Kpina062@users.noreply.github.com>
2020-05-12 10:27:45 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-var rotStatus = [];
-var cells = document.querySelectorAll("td");
-for (var i = 0; i < cells.length; i++) {
+const rotStatus = [];
+const cells = document.querySelectorAll("td");
+for (let i = 0; i < cells.length; i++) {
     rotStatus.push(0);
 }
 
 cells.forEach(function(t) {
     t.onclick = function () {
-        var i = 0;
+        let i = 0;
         cells.forEach(function(c) {
-            if (c != t){
-                if (rotStatus[i] == 1) {
+            if (c !== t){
+                if (rotStatus[i] === 1) {
                     c.setAttribute(`style`,`transform:rotate(0deg)`);
                     rotStatus[i] = 0;
                 }
             } else {
-                if (rotStatus[i] == 0) {
+                if (rotStatus[i] === 0) {
                     t.setAttribute(`style`,`transform:rotate(180deg)`);
                     rotStatus[i] = 1;
                 } else {
